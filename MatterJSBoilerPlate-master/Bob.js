f5a2f05ef950eb69d0038eb399abdd000d5f8a26
ayushpatelstudent <81617183+ayushpatelstudent@users.noreply.github.com>
2021-05-21 20:30:06 +0530

    Add files via upload

---
+++ MatterJSBoilerPlate-master/Bob.js
@@ -0,0 +1,23 @@
+class Bob{
+
+    constructor(x,y){
+        var options={
+            isStatic:false,
+            restitution:1,
+            friction:0,
+            density:7.3,
+        }
+        this.body=Bodies.circle(x,y,25,options);
+        World.add(world,this.body);
+    
+        console.log(this.body);
+    }
+    
+    
+    display(){
+    push()
+    ellipseMode(RADIUS);
+    ellipse(this.body.position.x,this.body.position.y,25,25);
+    pop()
+    }
+    }
